b19004806113930e6089205904d867e708892933
Khadra Mohamed <khadra.m123@gmail.com>
2021-03-26 18:02:04 -0400

    rearranged the code to work better

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,3 @@
-// This is the fight function that starts the game.
-var fight = function(){
-    window.alert("Welcome to the Robot Gladiators");
-};
-
-fight();
-
 //Player information
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -13,53 +6,68 @@ var playerMoney = 10;
 
 console.log(playerAttack, playerHealth, playerName, playerMoney);
 
-//Prompts the user to choose a condition.
-var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose. ")
-console.log(promptFight);
-
 //Enemy information 
 var enemyName = "Roberto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-//Conditions from user input in function promptFight
-if (promptFight === "fight" || promptFight==="FIGHT" || promptFight==="Fight") {
-    enemyHealth = enemyHealth - playerAttack;
-    console.log( playerName + " attacked " + enemyName + " now has " + enemyHealth + " health remaining.");
+// This is the fight function that starts the game.
+var fight = function(){
+    window.alert("Welcome to the Robot Gladiators");
 
-    if (enemyHealth <= 0){
-        window.alert(enemyName + " has died!");
-    }
-    else {
-        window.alert(enemyName + " still has "+ enemyHealth + " health left.");
-    }
+    //Prompts the user to choose a condition.
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose. ")
+    console.log(promptFight);
 
-    playerHealth = playerHealth -enemyAttack;
-    console.log(enemyName + " attacked " + playerName + " now has " + playerHealth + " health remaining.");
-
-    if (playerHealth <= 0){
-        window.alert(playerName + " has died!");
+    //if player chooses to fight
+    if (promptFight === "fight" || promptFight==="FIGHT" || promptFight==="Fight") {
+        //reduces enemyhealth from playerattack
+        enemyHealth = enemyHealth - playerAttack;
+        console.log( playerName + " attacked " + enemyName + " now has " + enemyHealth + " health remaining.");
+        
+        //Checks enemyhealth 
+        if (enemyHealth <= 0){
+            window.alert(enemyName + " has died!");
+        }
+        else {
+            window.alert(enemyName + " still has "+ enemyHealth + " health left.");
+        }
+        
+        //removes playerhealth from enemyattack
+        playerHealth = playerHealth -enemyAttack;
+        console.log(enemyName + " attacked " + playerName + " now has " + playerHealth + " health remaining.");
+    
+        //checks player's health
+        if (playerHealth <= 0){
+            window.alert(playerName + " has died!");
+        }
+        else {
+            window.alert(playerName + " still has "+ playerHealth + " health left.");
+        }
     }
-    else {
-        window.alert(playerName + " still has "+ playerHealth + " health left.");
+    
+    //if the player chooses to skip
+    else if (promptFight === "Skip" || promptFight==="SKIP" || promptFight==="skip"){
+        var confirmSkip = window.confirm ("Are you sure you'd like to quit?");
+    
+        if (confirmSkip){
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            playerMoney = playerMoney - 2;
+        }
+        else{
+            fight();
+        }
     }
-}
-
-else if (promptFight === "Skip" || promptFight==="SKIP" || promptFight==="skip"){
-    var confirmSkip = window.confirm ("Are you sure you'd like to quit?");
-
-    if (confirmSkip){
-        window.alert(playerName + " has decided to skip this fight. Goodbye!");
-        playerMoney = playerMoney -2;
-    }
+    
+    //if player inputs incorrect value
     else{
-        fight();
+        window.alert("You need to choose a valid option. Try again! ")
     }
-}
+};
 
-else{
-    window.alert("You need to choose a valid option. Try again! ")
-}
+//runs the fight function to start the game
+fight();
+
 
 
 
